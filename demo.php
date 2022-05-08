00c8d383358ea4291188514dd641c87058e2f7c0
Samuel Olaegbe <olaegbesamuel@gmail.com>
2022-05-08 21:21:37 +0100

    Submit and webhook url setup

--- demo.php
+++ demo.php
@@ -17,6 +17,10 @@ use Copyleaks\SubmissionRepository;
 use Copyleaks\SubmissionExclude;
 use Copyleaks\SubmissionPDF;
 use Copyleaks\CopyleaksAuthToken;
+use Copyleaks\CopyleaksExportModel;
+use Copyleaks\ExportCrawledVersion;
+use Copyleaks\ExportResults;
+use Copyleaks\ExportPdfReport;
 
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -25,8 +29,15 @@ class PlagiarismChecker
 {
     public Copyleaks $copyleaks;
     public CopyleaksAuthToken $authToken;
+    private string $scanId;
 
-    private const WEBHOOK_URL = "http://pra.local";
+    private const WEBHOOK_URL = "https://copyleaks.herokuapp.com/webhook";
+    private const RESULT_DOWNLOAD_URL = "https://copyleaks.herokuapp.com/download/";
+    private const RESULT_DOWNLOAD_URL_LOCAL = "http://copyleaks.test/download/";
+    private const WEBHOOK_URL_LOCAL = "http://copyleaks.test/webhook/";
+    private const COMPLETION_WEBHOOK_URL = "http://copyleaks.herokuapp.com/export/export-id/completed";
+    private const CRAWLED_WEBHOOK_URL = "http://copyleaks.herokuapp.com/export/export-id/crawled-version";
+    private const PDF_WEBHOOK_URL = "http://copyleaks.herokuapp.com/export/export-id/pdf-report";
 
     public function __construct()
     {
@@ -36,21 +47,24 @@ class PlagiarismChecker
         $this->authToken = $this->copyleaks->login($_ENV['API_EMAIL'], $_ENV['API_KEY']);
 
         error_log("Auth token " . print_r($this->authToken, true));
+
+        $this->scanId = "scanid123";
     }
 
     public function submit($filename, $url = false)
     {
+        error_log("submit called");
+
         $file = base64_encode(file_get_contents('C:/Users/HP/Documents/School/Year 3/Psychotherapy/180904061 Assignment.pdf'));
-        error_log(substr($file, 0, 100));
-        error_log(substr(chunk_split($file), 0, 100));
+        // error_log(substr(chunk_split($file), 0, 100));
         $submission = new CopyleaksFileSubmissionModel(
-            $file,
+            "aGVsbG8gd29ybGQ=",
             "assignment.pdf",
             new SubmissionProperties(
-                new SubmissionWebhooks(self::WEBHOOK_URL . "/{STATUS}"),
+                new SubmissionWebhooks(self::WEBHOOK_URL . "/{STATUS}/" . $this->scanId),
                 true, //include html (for PDFs yes)
                 null, // developer payload: none for now
-                true, // sandbox
+                false, // sandbox
                 6, // hours before copyleaks deletes the scan
                 1, // a fast scan with highest sensibility (1)
                 true,
@@ -64,11 +78,45 @@ class PlagiarismChecker
             )
         );
 
-        //time() should be replaced with scan id
-        $this->copyleaks->submitFile('education', $this->authToken, time(), $submission);
+        //time() should be replaced with scan id we will use to retrieve the result
+        $this->copyleaks->submitFile('businesses', $this->authToken, $this->scanId, $submission);
+
+        echo "Submitted successfully!";
     }
 
-}
+    public function webhook(CopyleaksAuthToken $authToken, $data)
+    {
+        error_log("Webhook called");
+        error_log("Data passed to our webhook " . print_r($data, true));
 
-$p = new PlagiarismChecker();
-$p->submit(true, true);
+        $cwebhook = str_replace("export-id", $data['id'], self::COMPLETION_WEBHOOK_URL);
+        $model = new CopyleaksExportModel(
+            $cwebhook,
+            array(new ExportResults("2a1b402420", self::RESULT_DOWNLOAD_URL_LOCAL . "/export-webhook/result/2a1b402420", "POST", array(array("key", "value")))),
+            new ExportCrawledVersion(self::RESULT_DOWNLOAD_URL_LOCAL . "/export-webhook/crawled-version", "POST", array(array("key", "value"))),
+            new ExportPdfReport(self::RESULT_DOWNLOAD_URL_LOCAL, "POST", array())
+        );
+
+        $exportedScanId = "abc";
+
+        $this->copyleaks->export($authToken, $exportedScanId, $exportedScanId, $model);
+
+        $request = $_SERVER['REQUEST_URI'];
+
+        echo json_encode(array(
+            'm' => 'o',
+            'model' => $model,
+            'request' => $request
+        ));
+    }
+
+    public function download()
+    {
+        error_log("download called");
+        $request = $_SERVER['REQUEST_URI'];
+
+        echo json_encode(array(
+            'request' => $request
+        ));
+    }
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once "demo.php";
+
+$class = new PlagiarismChecker();
+
+$endpoint = parse_url($_SERVER['REQUEST_URI']);
+$path = ltrim($endpoint['path'], "/");
+
+$url = $_SERVER['REQUEST_URI'];
+
+if (strpos($url, 'downloads') !== false) {
+    list($id, $filename) = explode("/", $path);
+    error_log('download endpoint');
+    $class->download();
+} elseif (strpos($url, 'submit') !== false) {
+    error_log('submit endpoint');
+    $class->submit(true, true);
+} elseif (strpos($url, 'webhook') !== false) {
+    error_log('webhook pinged!');
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    list($webhook, $status, $id) = explode("/", $path);
+
+    if ($status === 1) {
+        error_log('webhook completed!');
+        $class->webhook($class->authToken, $data);
+    }
+}
